1dc90716774fa7575a60d53d35bc7132261b069f
Erxyon <deal.guillaume@gmail.com>
2017-05-31 13:19:01 +0200

    Create calendar_define.js

---
+++ calendar_define.js
@@ -0,0 +1,42 @@
+var Periods = {
+  "After_School": "After School",
+  "Daytime": "Daytime",
+  "Evening": "Evening"
+};
+
+var Specials = {
+  "Crosswords": "Crosswords",
+  "Book_Train": "Read a book on the Train",
+  "TV_Quiz" : "TV Quiz",
+  "Curry" : "Curry possible",
+};
+
+var Films = {
+  "Shinjuku": [
+    {
+      name: "...",
+      starts: "...",
+      ends: "...",
+      effect: "..."
+    }
+  ]
+}
+
+var Answers = [
+  {
+    date: "...",
+    period: Periods.Morning
+    question: "...",
+    answer: "..."
+  }
+
+]
+
+var Exams = {
+
+}
+
+var Lotteries = {
+
+}
+
